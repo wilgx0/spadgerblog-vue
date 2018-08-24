b90d09b15a4e7d9c1b3d7236d1783d87b1b262f5
wilgx0 <396861557@qq.com>
2018-08-24 08:49:26 +0800

    '分页'

--- src/components/pages/Share.vue
+++ src/components/pages/Share.vue
@@ -15,11 +15,16 @@
                         v-model="loading"
                         :finished="finished"
                         @load="onLoad"
-                        :offset="10"
+                        :offset="20"
                 >
-                    <div class="list-item">
-                        <van-row v-for="item in list" :key="item">
-                            <van-col span="24">{{item.post_title}}</van-col>
+                    <div class="list-item" :style="{minHeight:itemHeight + 'px'}" v-for="(item,index) in list" :key="index">
+                        <van-row>
+                            <van-col span="24"><span class="title">{{item.post_title}}</span></van-col>
+                        </van-row>
+                        <van-row>
+                            <van-col span="10">作者</van-col>
+                            <van-col span="7">阅读</van-col>
+                            <van-col span="7">日期</van-col>
                         </van-row>
                     </div>
 
@@ -41,7 +46,8 @@
                 loading: false,           //是否处于加载状态
                 finished: false,          //是否已加载完所有数据
                 isLoading: false,         //是否处于下拉刷新状态
-                current_page:1,            //当前页数
+                current_page:0,            //当前页数
+                itemHeight:0,              //数据列表中各项的高度
             };
         },
         methods: {
@@ -53,52 +59,52 @@
                         url:url.getShareList,
                         method:'get',
                         params:{
-                            page:this.current_page ++
+                            page: ++ this.current_page
                         }
                     }).then(response=>{
                         this.loading = false;
-                        //console.log(response)
                         if(response.status == 200 ){
                             let data = response.data
                             let last_page = data.data.last_page
                             if (this.current_page >= last_page) {
-                                this.finished = true;
+                                this.finished = true
                             }
                             if(data.code == 1){
                                 this.list = this.list.concat(data.data.data)
+
                             } else {
                                 Toast.fail('服务器错误，未能请求到数据!');
+                                this.finished = true
                             }
                         } else {
                             Toast.fail('网络错误，未能请求到数据!');
+                            this.finished = true
                         }
+
                     }).catch(err=>{
                         this.loading = false;
+                        this.finished = true
                         console.log(err)
+
                     })
                 },500)
-                setTimeout(() => {
-                    for (let i = 0; i < 15; i++) {
-                        this.list.push(this.list.length + 1);
-                    }
-                    this.loading = false;
-                    if (this.list.length >= 60) {
-                        this.finished = true;
-                    }
-                }, 500);
+
             },
             onRefresh() {       //下拉刷新
                 setTimeout(() => {
-                    this.finished = false;
-                    this.isLoading = false;
+                    this.finished = false
+                    this.isLoading = false
                     this.list = []
-                    this.onLoad()
+                    this.current_page = 0
                 }, 500);
             }
         },
         mounted() {
-            let winHeight = document.documentElement.clientHeight   //视口高度
-            document.getElementById('list-content').style.height = (winHeight - 44 - 50) + 'px' //调整上拉加载框高度
+            let pageSize = 15
+            let winHeight = document.documentElement.clientHeight                               //视口高度
+            let loadHeight = winHeight - 44 - 50;                                               //视口高度减去头部和底部的高度
+            document.getElementById('list-content').style.height = loadHeight + 'px'            //调整上拉加载框高度
+            this.itemHeight = loadHeight / pageSize + 5
         }
     };
 </script>
@@ -107,6 +113,15 @@
     .share {
         .list-content {
             overflow: scroll;
+            .list-item{
+                font-size:14px;
+                padding: 0.5rem;
+                border-bottom: 2px solid #f0f0f0;
+                .title{
+                    color:#2e3135;
+                    font-weight:bold;
+                }
+            }
         }
     }
 </style>
